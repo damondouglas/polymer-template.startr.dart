4a327689182a5227861611b0e0bb1e29a9a513cd
Damon Douglas <douglas.damon@gmail.com>
2016-04-29 23:53:04 -0700

    Fix test top-level main() function takes arguments.

--- test/__projectname___test.dart
+++ test/__projectname___test.dart
@@ -3,7 +3,7 @@ library __projectname__.test;
 import 'package:test/test.dart';
 import 'package:__projectname__/__projectname__.dart' as __projectname__;
 
-main(List<String> args) {
+main() {
   group('foo', () {
     test('bar', (){
       expect(1,1);
